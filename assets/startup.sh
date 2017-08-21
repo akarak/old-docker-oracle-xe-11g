dd732b6678c60ec34ae7248e1a616deeb086b68c
akarak <alexey.karak@gmail.com>
2017-08-21 22:43:50 +0300

    Update startup.sh

--- assets/startup.sh
+++ assets/startup.sh
@@ -4,6 +4,7 @@
 chown -R oracle:dba /u01/app/oracle
 rm -f /u01/app/oracle/product
 ln -s /u01/app/oracle-product /u01/app/oracle/product
+
 # Update hostname
 sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 sed -i -E "s/PORT = [^)]+/PORT = 1521/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
@@ -12,126 +13,44 @@ echo "export PATH=\$ORACLE_HOME/bin:\$PATH" >> /etc/profile.d/oracle-xe.sh
 echo "export ORACLE_SID=XE" >> /etc/profile.d/oracle-xe.sh
 . /etc/profile
 
-impdp () {
-	DUMP_FILE=$(basename "$1")
-	DUMP_NAME=${DUMP_FILE%.dmp} 
-	cat > /tmp/impdp.sql << EOL
--- Impdp User
-CREATE USER IMPDP IDENTIFIED BY IMPDP;
-ALTER USER IMPDP ACCOUNT UNLOCK;
-GRANT dba TO IMPDP WITH ADMIN OPTION;
--- New Scheme User
-create or replace directory IMPDP as '/docker-entrypoint-initdb.d/';
-create tablespace $DUMP_NAME datafile '/u01/app/oracle/oradata/$DUMP_NAME.dbf' size 1000M autoextend on next 100M maxsize unlimited;
-create user $DUMP_NAME identified by $DUMP_NAME default tablespace $DUMP_NAME;
-alter user $DUMP_NAME quota unlimited on $DUMP_NAME;
-alter user $DUMP_NAME default role all;
-grant connect, resource to $DUMP_NAME;
-exit;
-EOL
+# Check for mounted database files
+if [ "$(ls -A /u01/app/oracle/oradata 2> /dev/null)" ]; then
+	echo "Using files in '/u01/app/oracle/oradata' instead of initial database."
+	echo "XE:$ORACLE_HOME:N" >> /etc/oratab
+	chown oracle:dba /etc/oratab
+	chown 664 /etc/oratab
+	printf "ORACLE_DBENABLED=false\nLISTENER_PORT=1521\nHTTP_PORT=8080\nCONFIGURE_RUN=true\n" > /etc/default/oracle-xe
+	rm -rf /u01/app/oracle-product/11.2.0/xe/dbs
+	ln -s /u01/app/oracle/dbs /u01/app/oracle-product/11.2.0/xe/dbs
+else
+	echo "Initializing database."
+	if [ -z "$CHARACTER_SET" ]; then
+		export CHARACTER_SET="AL32UTF8"
+	fi
 
-	su oracle -c "NLS_LANG=.$CHARACTER_SET $ORACLE_HOME/bin/sqlplus -S / as sysdba @/tmp/impdp.sql"
-	su oracle -c "NLS_LANG=.$CHARACTER_SET $ORACLE_HOME/bin/impdp IMPDP/IMPDP directory=IMPDP dumpfile=$DUMP_FILE $IMPDP_OPTIONS nologfile=y"
-	#Disable IMPDP user
-	echo -e 'ALTER USER IMPDP ACCOUNT LOCK;\nexit;' | su oracle -c "NLS_LANG=.$CHARACTER_SET $ORACLE_HOME/bin/sqlplus -S / as sysdba"
-}
+	mv /u01/app/oracle-product/11.2.0/xe/dbs /u01/app/oracle/dbs
+	ln -s /u01/app/oracle/dbs /u01/app/oracle-product/11.2.0/xe/dbs
 
-impFile() {
-	echo "found file $1"
-	case "$1" in
-		*.sh)     echo "[IMPORT] $0: running $1"; . "$1" ;;
-		*.sql)    echo "[IMPORT] $0: running $1"; echo "exit" | su oracle -c "NLS_LANG=.$CHARACTER_SET $ORACLE_HOME/bin/sqlplus -S / as sysdba @$1"; echo ;;
-		*.dmp)    echo "[IMPORT] $0: running $1"; impdp $1 ;;
-		*)        echo "[IMPORT] $0: ignoring $1" ;;
-	esac
-}
+	printf 8080\\n1521\\n${DEFAULT_SYS_PASS}\\n${DEFAULT_SYS_PASS}\\ny\\n | service oracle-xe configure
+	echo "Setting 'sys/system' passwords"
+	echo  alter user sys identified by \"$DEFAULT_SYS_PASS\"\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
+	echo  alter user system identified by \"$DEFAULT_SYS_PASS\"\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
 
-case "$1" in
-	'')
-		# Check for mounted database files
-		if [ "$(ls -A /u01/app/oracle/oradata 2> /dev/null)" ]; then
-			echo "Using files in '/u01/app/oracle/oradata' instead of initial database."
-			echo "XE:$ORACLE_HOME:N" >> /etc/oratab
-			chown oracle:dba /etc/oratab
-			chown 664 /etc/oratab
-			printf "ORACLE_DBENABLED=false\nLISTENER_PORT=1521\nHTTP_PORT=8080\nCONFIGURE_RUN=true\n" > /etc/default/oracle-xe
-			rm -rf /u01/app/oracle-product/11.2.0/xe/dbs
-			ln -s /u01/app/oracle/dbs /u01/app/oracle-product/11.2.0/xe/dbs
-		else
-			echo "Initializing database."
+	echo "Database initialized."
+	echo "Please visit http://#containeer:8080/apex to proceed with configuration."
+fi
 
-			if [ -z "$CHARACTER_SET" ]; then
-				export CHARACTER_SET="AL32UTF8"
-			fi
+service oracle-xe start
 
-			printf "Setting up:\nprocesses=$processes\nsessions=$sessions\ntransactions=$transactions\n"
-			echo "If you want to use different parameters set processes, sessions, transactions env variables and consider this formula:"
-			printf "processes=x\nsessions=x*1.1+5\ntransactions=sessions*1.1\n"
+if [ "$ORACLE_ALLOW_REMOTE" = true ]; then
+	echo alter system disable restricted session\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
+fi
 
-			mv /u01/app/oracle-product/11.2.0/xe/dbs /u01/app/oracle/dbs
-			ln -s /u01/app/oracle/dbs /u01/app/oracle-product/11.2.0/xe/dbs
-
-			#Setting up processes, sessions, transactions.
-			sed -i -E "s/processes=[^)]+/processes=$processes/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/init.ora
-			sed -i -E "s/processes=[^)]+/processes=$processes/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/initXETemp.ora
-			
-			sed -i -E "s/sessions=[^)]+/sessions=$sessions/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/init.ora
-			sed -i -E "s/sessions=[^)]+/sessions=$sessions/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/initXETemp.ora
-
-			sed -i -E "s/transactions=[^)]+/transactions=$transactions/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/init.ora
-			sed -i -E "s/transactions=[^)]+/transactions=$transactions/g" /u01/app/oracle/product/11.2.0/xe/config/scripts/initXETemp.ora
-
-			printf 8080\\n1521\\n${DEFAULT_SYS_PASS}\\n${DEFAULT_SYS_PASS}\\ny\\n | service oracle-xe configure
-			echo "Setting 'sys/system' passwords"
-			echo  alter user sys identified by \"$DEFAULT_SYS_PASS\"\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
-   			echo  alter user system identified by \"$DEFAULT_SYS_PASS\"\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
-
-			echo "Database initialized."
-			echo "Please visit http://#containeer:8080/apex to proceed with configuration."
-		fi
-
-
-		service oracle-xe start
-
-		if [ "$ORACLE_ALLOW_REMOTE" = true ]; then
-			echo alter system disable restricted session\; | su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" > /dev/null 2>&1
-		fi
-
-
-		echo "Starting import scripts from '/docker-entrypoint-initdb.d':"
-
-		for fn in $(ls -1 /docker-entrypoint-initdb.d/* 2> /dev/null)
-		do
-			# execute script if it didn't execute yet or if it was changed
-			cat /docker-entrypoint-initdb.d/.cache 2> /dev/null | grep "$(md5sum $fn)" || impFile $fn
-		done
-
-		# clear cache
-		if [ -e /docker-entrypoint-initdb.d/.cache ]; then
-			rm /docker-entrypoint-initdb.d/.cache
-		fi
-
-		# regenerate cache
-		ls -1 /docker-entrypoint-initdb.d/*.sh 2> /dev/null | xargs md5sum >> /docker-entrypoint-initdb.d/.cache
-		ls -1 /docker-entrypoint-initdb.d/*.sql 2> /dev/null | xargs md5sum >> /docker-entrypoint-initdb.d/.cache
-		ls -1 /docker-entrypoint-initdb.d/*.dmp 2> /dev/null | xargs md5sum >> /docker-entrypoint-initdb.d/.cache
-
-		echo "Import finished"
-		echo
-
-		echo "Database ready to use. Enjoy! ;)"
-
-		##
-		## Workaround for graceful shutdown.
-		##
-		while [ "$END" == '' ]; do
-			sleep 1
-			trap "service oracle-xe stop && END=1" INT TERM
-		done
-		;;
-
-	*)
-		echo "Database is not configured. Please run 'service oracle-xe configure' if needed."
-		exec "$@"
-		;;
-esac
+for f in /docker-entrypoint-initdb.d/*; do
+  case "$f" in
+    *.sh)     echo "$0: running $f"; . "$f" ;;
+    *.sql)    echo "$0: running $f"; su oracle -s /bin/bash -c "$ORACLE_HOME/bin/sqlplus -s / as sysdba" @"$f"; echo ;;
+    *)        echo "$0: ignoring $f" ;;
+  esac
+  echo
+done
